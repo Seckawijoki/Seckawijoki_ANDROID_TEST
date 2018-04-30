e9ec0452c186da40180b6d203aa00f7326e7c834
seckawijoki <seckawijoki@gmail.com>
2018-04-30 13:39:08 +0800

    Windows 10 update test.

---
+++ app/src/main/java/com/seckawijoki/android_test/Windows10Tag.java
@@ -0,0 +1,9 @@
+package com.seckawijoki.android_test;
+
+/**
+ * Created by 瑶琴频曲羽衣魂 on 2018/4/30 at 13:37.
+ * * Testing for available update in Ubuntu Android Studio from Windows 10s'.
+ */
+
+final public class Windows10Tag {
+}
