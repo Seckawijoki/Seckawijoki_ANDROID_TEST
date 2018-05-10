24f973e838404d6455ed3b632a7093db09b46152
seckawijoki <seckawijoki@gmail.com>
2018-05-10 23:57:01 +0800

    Windows 10 update test.

---
+++ app/src/main/java/com/seckawijoki/android_test/Windows10Commitment.java
@@ -0,0 +1,7 @@
+package com.seckawijoki.android_test;
+
+/**
+ * Created by 瑶琴频曲羽衣魂 on 2018/5/10 at 23:56 under Windows-10 Professional.
+ */
+final public class Windows10Commitment {
+}
--- app/src/main/java/com/seckawijoki/android_test/Windows10Tag.java
+++
@@ -1,9 +0,0 @@
-package com.seckawijoki.android_test;
-
-/**
- * Created by 瑶琴频曲羽衣魂 on 2018/4/30 at 13:37.
- * * Testing for available update in Ubuntu Android Studio from Windows 10s'.
- */
-
-final public class Windows10Tag {
-}
