2e8c1411c9bf2691aa91c9f86e90f3be29135c14
seckawijoki <seckawijoki@gmail.com>
2018-05-29 18:42:07 +0800

    A commitment from ubuntu in sake of testing for update under windows.

--- app/src/main/java/com/seckawijoki/android_test/UbuntuCommitment.java
+++ app/src/main/java/com/seckawijoki/android_test/UbuntuCommitment.java
@@ -3,5 +3,6 @@ package com.seckawijoki.android_test;
 /**
  * Created by seckawijoki on 2018-05-10 at 23:30 under Ubuntu-16.04 LTS.
  */
-public final class UbuntuCommitment {
+interface UbuntuCommitment {
+  // TODO: 18-5-29 change to interface
 }
