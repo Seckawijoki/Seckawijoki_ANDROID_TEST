18d13e49629d7464f761181afa2463c417930443
seckawijoki@gmail.com <seckawijoki@gmail.com>
2018-06-01 18:36:34 +0800

    ndk first try

--- app/src/main/java/com/seckawijoki/android_test/activity/ADialogActivity.java
+++ app/src/main/java/com/seckawijoki/android_test/activity/ADialogActivity.java
@@ -5,7 +5,6 @@ import com.seckawijoki.android_test.base.AbsLifecycleLogActivity;
 /**
  * Created by 瑶琴频曲羽衣魂 on 2018/4/20 at 16:41.
  */
-
 public class ADialogActivity extends AbsLifecycleLogActivity {
   @Override
   public String setTag() {
---
+++ app/src/main/java/com/seckawijoki/android_test/ndk/JavaInvokeCppTest.java
@@ -0,0 +1,7 @@
+package com.seckawijoki.android_test.ndk;
+
+/**
+ * Created by Seckawijoki on 2018/6/1 at 15:32 under Windows10 Professional.
+ */
+public class JavaInvokeCppTest {
+}
