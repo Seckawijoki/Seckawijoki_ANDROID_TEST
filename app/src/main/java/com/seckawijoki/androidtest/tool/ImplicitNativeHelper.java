bf6fb0c2a74377a44ff89cade7bf19cd67037331
seckawijoki@gmail.com <seckawijoki@gmail.com>
2018-06-07 10:27:01 +0800

    Complete commitment of NDK manual building with NDK r10e version.

--- app/src/main/java/com/seckawijoki/androidtest/tool/ImplicitNativeHelper.java
+++ app/src/main/java/com/seckawijoki/androidtest/tool/ImplicitNativeHelper.java
@@ -3,7 +3,7 @@ package com.seckawijoki.androidtest.tool;
 import android.util.Log;
 
 /**
- * Created by Seckawijoki on 2018/6/6 at 15:53 under Windows10 Professional.
+ * Created by Seckawijoki on 2018/6/6 at 15:53 under Windows-10 Professional.
  */
 public class ImplicitNativeHelper {
   private static final String TAG = "ImplicitNativeHelper";
